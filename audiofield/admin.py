8e9770245ffda884ae3d9dcc4cac8e70f040a14a
Stephen Wolff <stephen@maxgatedigital.com>
2021-03-25 16:16:48 +0000

    Comment out AudioFileAdmin, as using project admin for audio files

--- audiofield/admin.py
+++ audiofield/admin.py
@@ -11,36 +11,36 @@
 # Arezqui Belaid <info@star2billing.com>
 #
 
-from django.contrib import admin
-from audiofield.models import AudioFile
-from django.utils.translation import ugettext_lazy as _
-import os
+# from django.contrib import admin
+# from audiofield.models import AudioFile
+# from django.utils.translation import ugettext_lazy as _
+# import os
 
 
-class AudioFileAdmin(admin.ModelAdmin):
-    """Allows the administrator to view and modify uploaded audio files"""
+# class AudioFileAdmin(admin.ModelAdmin):
+#     """Allows the administrator to view and modify uploaded audio files"""
+#
+#     list_display = ('id', 'name', 'audio_file_player', 'created_date', 'user')
+#     # list_display_links = ['id', 'name',]
+#     ordering = ('id', )
+#
+#     actions = ['custom_delete_selected']
+#
+#     def custom_delete_selected(self, request, queryset):
+#         # custom delete code
+#         n = queryset.count()
+#         for i in queryset:
+#             if i.audio_file:
+#                 if os.path.exists(i.audio_file.path):
+#                     os.remove(i.audio_file.path)
+#             i.delete()
+#         self.message_user(request, _("Successfully deleted %d audio files.") % n)
+#     custom_delete_selected.short_description = "Delete selected items"
+#
+#     def get_actions(self, request):
+#         actions = super(AudioFileAdmin, self).get_actions(request)
+#         del actions['delete_selected']
+#         return actions
 
-    list_display = ('id', 'name', 'audio_file_player', 'created_date', 'user')
-    # list_display_links = ['id', 'name',]
-    ordering = ('id', )
 
-    actions = ['custom_delete_selected']
-
-    def custom_delete_selected(self, request, queryset):
-        # custom delete code
-        n = queryset.count()
-        for i in queryset:
-            if i.audio_file:
-                if os.path.exists(i.audio_file.path):
-                    os.remove(i.audio_file.path)
-            i.delete()
-        self.message_user(request, _("Successfully deleted %d audio files.") % n)
-    custom_delete_selected.short_description = "Delete selected items"
-
-    def get_actions(self, request):
-        actions = super(AudioFileAdmin, self).get_actions(request)
-        del actions['delete_selected']
-        return actions
-
-
-admin.site.register(AudioFile, AudioFileAdmin)
+#admin.site.register(AudioFile, AudioFileAdmin)
